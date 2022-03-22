c07c08e3b9dfad0fc037e4c3e90fcac944458fd2
Artyom Zhukovsky <Artyom.Zhukovsky@dip.co.uk>
2022-03-22 10:33:11 +0300

    replaced file

---
+++ MyCollection.cs
@@ -0,0 +1,19 @@
+﻿namespace MyClass
+{
+    public class MyCollection<T>
+    {
+        List<T> Collection = new();
+
+        public int CollectionLength { 
+            get { return Collection.Count; }
+        }
+        public void Add(T item) {
+            Collection.Add(item);
+        }
+
+        public T GetItemByIndex(int index)
+        {
+            return Collection[index];  
+        }
+    }
+}
